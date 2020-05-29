81dff547c2015222a4a58835f06ecb09ef1305da
ArnoCo <acognard@gmail.com>
2020-05-29 16:56:14 -0700

    Add JoinList

--- APIUtil.js
+++ APIUtil.js
@@ -20,18 +20,16 @@ module.exports = {
         const character = await api_helper.make_API_call(apiKeyId)
             .then(response => {
                 return response.data.results[0]
-                // return JSON.stringify(response.data.results[0], null, 2)
             })
             .catch(error => {
                 console.log(error);
             });
 
-        return character    
-        // const characterComicInfo = await addCi(character)    
+        return character     
     },
     fetchComicsById: async function (id, num) {
         if (num%100 !== 0) {
-            num=((Math.abs(num) / 100) +1) * 100
+            num=((Math.floor(num / 100)) +1) * 100
         }
         console.log(num)
 
@@ -51,11 +49,68 @@ module.exports = {
                     console.log(error);
                 });
 
-        }
-        console.log(comics)   
+        } 
         return comics    
+    },
+    fetchComicsTitlesById: async function (id, num) {
+
+        let comics = [];
+
+        for (let i = 0; i <= num; i += 100) {
+            let apiKeyIdComics = `https://gateway.marvel.com:443/v1/public/characters/${id}/comics?limit=100&offset=${i}&ts=${Date.now()}&apikey=${keys.public}&hash=${keys_helper.hashedKeys()}`
+            await api_helper.make_API_call(apiKeyIdComics)
+                .then(response => {
+                    let data = response.data.results
+                    for (let j = 0; j < data.length; j++) {
+                        comics.push(data[j].title)
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+
+        }
+        // console.log(comics)
+        return comics
+    },
+    fetchJoinComicList: async function (id1, num1, id2, num2) {
+        if (num1 % 100 !== 0) {
+            num1 = ((Math.floor(num1 / 100)) + 1) * 100
+        }
+
+        if (num2 % 100 !== 0) {
+            num2 = ((Math.floor(num2 / 100)) + 1) * 100
+        }
+
+        let comicsCounter = {};
+
+        const comicsTitlesList1 = await this.fetchComicsTitlesById(id1, num1)
+            .then(response => {
+                response.forEach(title =>{
+                    if (!comicsCounter[title]) {
+                        comicsCounter[title] = 1
+                    } else {
+                        comicsCounter[title] += 1
+                    }
+                })
+                return comicsCounter
+            })
+        
+        const comicsTitlesList2 = await this.fetchComicsTitlesById(id2, num2)
+            .then(response => {
+                response.forEach(title => {
+                    if (!comicsCounter[title]) {
+                        comicsCounter[title] = 1
+                    } else {
+                        comicsCounter[title] += 1
+                    }
+                })
+                return comicsCounter
+            })    
+
+        const joinList = Object.keys(comicsCounter).filter(key => comicsCounter[key] > 1)
+        return joinList;
     }
 }
-
 
 
--- character_finder.js
+++ character_finder.js
@@ -1,6 +1,3 @@
-const axios = require('axios');
-const keys_helper = require('./keys_helper')
-const keys = require('./keys')
 const APIUtil = require('./APIUtil')
 
 
@@ -20,34 +17,44 @@ CharacterFinder.prototype.getCharacterByName = async function(name) {
         console.log(error);
     });
     console.log(character.name)
+    console.log(character.id)
+    console.log(character.comics.available)
     return character
 }
 
 CharacterFinder.prototype.getComicsById = async function(id, num) {
-    let comics = await APIUtil.fetchComicsById(id, num)
+    let comics = await APIUtil.fetchComicsTitlesById(id, num)
         .then(response => {
             return response
         })
         .catch(error => {
             console.log(error);
         });
+    console.log(typeof comics)
+    return comics
+}
+
+CharacterFinder.prototype.getJoinComicList = async function(id1, num1, id2, num2) {
+    let comics = await APIUtil.fetchJoinComicList(id1, num1, id2, num2)
+        .then(response => {
+            return response
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    console.log(typeof comics)
     console.log(comics)
+    console.log(comics.length)
     return comics
 }
 
 
 
 let c = new CharacterFinder('Emma Frost');
+// c.getCharacterByName("Storm")
+// c.getCharacterByName("Emma Frost")
+c.getCharacterByName("Storm")
 c.getCharacterByName("Emma Frost")
-c.getComicsById(1009310, 392)
-
-async function foo() {
-    try {
-        const result = await doSomething();
-        const newResult = await doSomethingElse(result);
-        const finalResult = await doThirdThing(newResult);
-        console.log(`Got the final result: ${finalResult}`);
-    } catch (error) {
-        failureCallback(error);
-    }
-}
+// c.getComicsById(1009310, 392)
+c.getJoinComicList(1009629, 829, 1009310, 392)
+// c.getJoinComicList(1009695, 30, 1009696, 49)
